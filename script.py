533e53db53f0fe4af331d3d578e004bc51b65c7c
StanHannebelle <45121661+StanHannebelle@users.noreply.github.com>
2019-06-10 19:38:53 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,4 @@
-import numpy as np
-import iris
-import iris_grib
-cubes = iris_grib.load_cubes('grib.grb2')
-cubes=list(cubes)
-print(np.shape(cubes[0].data))
+import os
+
+for param in os.environ.keys():
+    print "%20s %s" % (param,os.environ[param])
